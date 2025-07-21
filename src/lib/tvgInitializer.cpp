JSUYA/thorvg

infra: Add window build test in CI
Add build test for normal build

src/lib/tvgInitializer.cpp
@@ -23,6 +23,10 @@
 #include "tvgTaskScheduler.h"
 #include "tvgLoader.h"
 
+#ifdef _MSC_VER
+    #include <cstring>
+#endif
+
 #ifdef THORVG_SW_RASTER_SUPPORT
     #include "tvgSwRenderer.h"
 #endif

open-vela/external_thorvg

src/lib/tvgInitializer.cpp
@@ -23,6 +23,10 @@
 #include "tvgTaskScheduler.h"
 #include "tvgLoader.h"
 
+#ifdef _WIN32
+    #include <cstring>
+#endif
+
 #ifdef THORVG_SW_RASTER_SUPPORT
     #include "tvgSwRenderer.h"
 #endif